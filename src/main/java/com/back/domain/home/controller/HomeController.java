637604f570749fdf74f1b7aaba47fa3552df7919
hansol0118 <wall197944@naver.com>
2026-03-13 15:11:14 +0900

    57

--- src/main/java/com/back/domain/home/controller/HomeController.java
+++ src/main/java/com/back/domain/home/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.back.domain.home.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.net.UnknownHostException;
 public class HomeController {
 
     @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
+    @Tag(name="Home",description = "홈페이지")
     public String home() throws UnknownHostException {
 
         InetAddress localhost = InetAddress.getLocalHost();
